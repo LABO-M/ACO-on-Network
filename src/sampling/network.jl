e612c7fd0bf1e1de754779835ddc1ee84d0b161f
ShoSampo <shosampo@gmail.com>
2024-02-21 16:17:16 +0900

    network_popularity関数の人気度更新を最適化

--- src/sampling/network.jl
+++ src/sampling/network.jl
@@ -18,6 +18,24 @@ function initialize_network(T::Int, r::Int)
     return k_in, k_out, link_matrix
 end
 
+function initialize_network(T::Int, r::Int, omega::Float64)
+    k_in = zeros(Int, T)
+    k_out = zeros(Int, T)
+    link_matrix = zeros(Int, T, r)
+    l = zeros(Float64, T)
+
+    k_out[1] = r
+
+    for t in 2:(r + 1)
+        k_in[t] = t - 1
+        k_out[t] = r - (t - 1)
+        link_matrix[t, 1:(t - 1)] .= 1:(t - 1)
+        l[t] = k_in[t] + omega * k_out[t]
+    end
+
+    return k_in, k_out, link_matrix, l
+end
+
 function generate_network(T::Int, r::Int, omega::Float64)
     if omega == -1.0
         return network_lattice(T, r)
@@ -28,14 +46,10 @@ end
 
 function network_popularity(T::Int, r::Int, omega::Float64)
     # 初期条件の設定
-    k_in, k_out, link_matrix = initialize_network(T, r)
-    l = zeros(Float64, T)
+    k_in, k_out, link_matrix, l = initialize_network(T, r, omega)
 
     # ネットワークの進化
     for t in (r + 2):T
-        # 人気度の更新
-        l .= k_in .+ omega .* k_out
-
         # 人気度が0より大きいアリを選択可能なリストに追加
         popular_ants = findall(x -> x > 0, l)
 
@@ -49,12 +63,17 @@ function network_popularity(T::Int, r::Int, omega::Float64)
         probabilities = l[popular_ants] / total_popularity
         selected_ants = sample(popular_ants, Weights(probabilities), num_links, replace=false)
 
+        # 新しいリンクが追加されるノードの人気度更新
+        l[t] = k_in[t] + omega * k_out[t]
+
         # リンクの追加と出次数の更新
         for ant in selected_ants
             link_slot = findfirst(x -> x == 0, link_matrix[t, :])
             link_matrix[t, link_slot] = ant
             k_out[ant] += 1
             k_in[t] += 1
+            # 選択されたアリの人気度更新
+            l[ant] = k_in[ant] + omega * k_out[ant]
         end
     end
 
